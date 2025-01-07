c6cc36c2bb70f74f973c45d0619bb432d845ee2b
5tr1ker <tjseocld@naver.com>
2025-01-07 23:33:52 +0900

    init: Spring Boot 프로젝트 생성
    init: Spring Boot 프로젝트 생성

---
+++ boot/src/test/java/com/study/sjpark/SjparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.sjpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
