bd600022caa1e902446c37b7f34afd4781c574a4
5tr1ker <follow2272@naver.com>
2025-09-01 22:04:26 +0900

    feat: 3장 연산자 학습 내용 추가 [ 산술 연산자 ]
    feat: 3장 연산자 학습 내용 추가

---
+++ java/src/c_operator/Operator.java
@@ -0,0 +1,12 @@
+package c_operator;
+
+public class Operator {
+
+    public void OperatorEx1() {
+        byte a = 10;
+        byte b = 20;
+        byte c = (byte) (a + b);
+        System.out.println(c);
+    }
+
+}
