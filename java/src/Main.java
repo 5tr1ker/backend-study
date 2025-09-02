cb76772be9ef21dd65824dbfecf18993fd5d0c73
5tr1ker <follow2272@naver.com>
2025-09-02 22:14:18 +0900

    feat: 3장 연산자 학습 결과 반영 [ char의 문자, 실수 연산 및 나눗셈의 소수점 처리 ]
    feat: 3장 연산자 학습 결과 반영 [ char의 문자, 실수 연산 및 나눗셈의 소수점 처리 ]

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,5 +1,8 @@
+import c_operator.Operator;
+
 public class Main {
     public static void main(String[] args) {
-
+        Operator operator = new Operator();
+        operator.OperatorEx3();
     }
 }
--- java/src/c_operator/Operator.java
+++ java/src/c_operator/Operator.java
@@ -9,4 +9,18 @@ public class Operator {
         System.out.println(c);
     }
 
+    public void OperatorEx2() {
+        char a = 'a';
+        char b = 'a' + 1;
+        //char c =  (a + b); 오류
+        char c =  (char) (a + b);
+        System.out.println(c);
+    }
+
+    public void OperatorEx3() {
+        System.out.println(3142.0 / 1000);
+
+        System.out.println(3142.0 / 1000.0);
+    }
+
 }
