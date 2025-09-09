e539a52468e9aaa76ffbbbf02a0f0b7d71f3c7d7
5tr1ker <follow2272@naver.com>
2025-09-09 22:00:43 +0900

    feat: Switch 문 제약조건 및 활용 학습
    feat: Switch 문 제약조건 및 활용 학습

--- java/src/d_statement/Statement.java
+++ java/src/d_statement/Statement.java
@@ -3,16 +3,34 @@ package d_statement;
 public class Statement {
 
     public void StatementEx1() {
+        final int THREE = 3;
+
         switch (3) {
             case 1:
                 System.out.println("this is 1");
             case 2:
                 System.out.println("this is 2");
-            case 3:
+            case THREE:
                 System.out.println("this is 3");
             case 4:
                 System.out.println("this is 4");
-            case 5:
+            case 'a':
+                System.out.println("this is 5");
+        }
+    }
+
+    public void StatementEx2() {
+        switch ("a") {
+            case "b":
+                System.out.println("this is 1");
+            case "c":
+                System.out.println("this is 2");
+            case "d":
+                System.out.println("this is 3");
+            case "a":
+                System.out.println("this is 4");
+            //case "b":
+            case "ab":
                 System.out.println("this is 5");
         }
     }
