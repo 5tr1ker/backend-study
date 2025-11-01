3b6248c0bbf3b492ff92ec4c5a762b28d3c44adf
5tr1ker <follow2272@naver.com>
2025-11-01 12:33:09 +0900

    feat: Wrapper 클래스 , 오토 박싱과 언박싱 학습

--- java/src/h_util/Main_2.java
+++ java/src/h_util/Main_2.java
@@ -1,47 +1,76 @@
 package h_util;
 
+import java.util.ArrayList;
+
 public class Main_2 {
 
     public static void main(String[] args) {
+        // 2
+//        Integer t1 = new Integer("3");
+//        Integer t2 = new Integer("3");
+//        System.out.println(t1 == t2);
+//        System.out.println(t1.equals(t2));
+//        System.out.println(t1.compareTo(t2));
+//
+//        System.out.println(Integer.valueOf("FF", 16));
+//
+//        Integer t3 = new Integer("3");
+//        int t4 = 5;
+//        System.out.println(t3.intValue() + t4);
+//
+//        ArrayList<Integer> arrayList = new ArrayList<>();
+//        arrayList.add(10); // 오토 박싱 10 -> new Integer(10);
+//        int result = arrayList.get(0); // 오토 언박싱 new Integer(10) -> 10
+//
+//        Integer t5 = new Integer(5);
+//        Long t6 = new Long(20L);
+//
+//        long t7 = t5 + t6;
+//        System.out.println(t7);
+//
+//        int t8 = (int)t5;
+//        Integer t9 = (Integer)t8;
+//        Object obj;
+
         // Math 클래스
-        double value = 12.3456712;
-        value *= (1000);
-        System.out.println(Math.round(value) / 1000.0 );
-
-        System.out.println(Math.rint(12.345));
-        System.out.println(Math.rint(-1.5) + " : " + Math.round(-1.5));
-        System.out.println(Math.rint(-1.3) + " : " + Math.round(-1.3));
-        System.out.println(Math.rint(-1.8) + " : " + Math.round(-1.8));
-        System.out.println(Math.rint(1.5) + " : " + Math.round(1.5));
-        System.out.println(Math.rint(1.3) + " : " + Math.round(1.3));
-        System.out.println(Math.rint(1.8) + " : " + Math.round(1.8));
-
-        // 오버 플로우 발생
-        System.out.println(Math.addExact(10, 50));
-        //System.out.println(Math.addExact(2_000_000_000 , 2_000_000_000));
-        //System.out.println(Math.multiplyExact(123123, 124124));
-        //System.out.println(Math.negateExact(Integer.MIN_VALUE));
-
-        System.out.println(Math.sqrt(9.0));
-        System.out.println(Math.pow(3 , 3));
-
-
-
-        StringBuffer sb = new StringBuffer();
-        sb.append("abc").append("de");
-
-        StringBuffer sb2 = new StringBuffer("abc").append("de");
-        System.out.println("sb == sb2 : " + (sb==sb2));
-        System.out.println("sb equals sb2 : " + (sb.equals(sb2)));
-        System.out.println("sb str == sb2 str : " + (sb.toString().equals(sb.toString())));
-
-        StringBuffer sb3 = new StringBuffer(32);
-        System.out.println("버퍼 크기 : " + sb3.capacity());
-        System.out.println("문자열 추가 : " + sb3.append("hello"));
-        System.out.println("문자열 사이 추가 : " + sb3.insert(2 , " he"));
-        System.out.println("문자열 사이 제거 : " + sb3.delete(5,32));
-        System.out.println("문자열 바꾸기 : " + sb3.replace(0 , 2 , "sui"));
-        System.out.println("문자열 뒤바꾸기 : " + sb3.reverse());
-        System.out.println(sb);
+//        double value = 12.3456712;
+//        value *= (1000);
+//        System.out.println(Math.round(value) / 1000.0 );
+//
+//        System.out.println(Math.rint(12.345));
+//        System.out.println(Math.rint(-1.5) + " : " + Math.round(-1.5));
+//        System.out.println(Math.rint(-1.3) + " : " + Math.round(-1.3));
+//        System.out.println(Math.rint(-1.8) + " : " + Math.round(-1.8));
+//        System.out.println(Math.rint(1.5) + " : " + Math.round(1.5));
+//        System.out.println(Math.rint(1.3) + " : " + Math.round(1.3));
+//        System.out.println(Math.rint(1.8) + " : " + Math.round(1.8));
+//
+//        // 오버 플로우 발생
+//        System.out.println(Math.addExact(10, 50));
+//        //System.out.println(Math.addExact(2_000_000_000 , 2_000_000_000));
+//        //System.out.println(Math.multiplyExact(123123, 124124));
+//        //System.out.println(Math.negateExact(Integer.MIN_VALUE));
+//
+//        System.out.println(Math.sqrt(9.0));
+//        System.out.println(Math.pow(3 , 3));
+//
+//
+//
+//        StringBuffer sb = new StringBuffer();
+//        sb.append("abc").append("de");
+//
+//        StringBuffer sb2 = new StringBuffer("abc").append("de");
+//        System.out.println("sb == sb2 : " + (sb==sb2));
+//        System.out.println("sb equals sb2 : " + (sb.equals(sb2)));
+//        System.out.println("sb str == sb2 str : " + (sb.toString().equals(sb.toString())));
+//
+//        StringBuffer sb3 = new StringBuffer(32);
+//        System.out.println("버퍼 크기 : " + sb3.capacity());
+//        System.out.println("문자열 추가 : " + sb3.append("hello"));
+//        System.out.println("문자열 사이 추가 : " + sb3.insert(2 , " he"));
+//        System.out.println("문자열 사이 제거 : " + sb3.delete(5,32));
+//        System.out.println("문자열 바꾸기 : " + sb3.replace(0 , 2 , "sui"));
+//        System.out.println("문자열 뒤바꾸기 : " + sb3.reverse());
+//        System.out.println(sb);
     }
 }
