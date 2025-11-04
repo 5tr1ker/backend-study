27893f67ee9c479bd0782ccbd49e645dc0029bdd
5tr1ker <follow2272@naver.com>
2025-11-04 22:08:00 +0900

    feat: Map.Entry 인터페이스 및 ArrayList 학습

--- java/src/i_collection/Main.java
+++ java/src/i_collection/Main.java
@@ -1,9 +1,21 @@
 package i_collection;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        ArrayList<Integer> ar = new ArrayList<>();
+        ar.add(1);
+        ar.add(2);
+        ar.add(2);
+        ar.add(3);
+        for(int i = 0; i < ar.size(); i++){
+            if(ar.get(i) % 2 == 0) {
+                ar.remove(i);
+            }
+        }
+        System.out.println(ar);
     }
 
 }
