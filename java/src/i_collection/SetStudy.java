91e726224b821968c170eecc1e89f30b7135539a
5tr1ker <follow2272@naver.com>
2025-11-14 12:17:48 +0900

    feat: HashMap 학습

--- java/src/i_collection/SetStudy.java
+++ java/src/i_collection/SetStudy.java
@@ -33,23 +33,24 @@ class Person {
 public class SetStudy {
 
     public static void main(String[] args) {
-//        for(int z = 0; z < 10; z++) {
-//            HashSet<Integer> set = new HashSet<>();
-//            LinkedHashSet<Integer> set = new LinkedHashSet<>();
-//            for(int i = 0; i < 10; i++) {
-//                set.add((int) (Math.random() * 5) + 1);
-//            }
-//
-//            System.out.println(set);
-//        }
+        /*
+        for(int z = 0; z < 10; z++) {
+            HashSet<Integer> set = new HashSet<>();
+            LinkedHashSet<Integer> set = new LinkedHashSet<>();
+            for(int i = 0; i < 10; i++) {
+                set.add((int) (Math.random() * 5) + 1);
+            }
 
-//        HashSet<Person> people = new HashSet<>();
-//        for(int i = 0; i < 10; i++) {
-//            Person person = new Person((int) (Math.random() * 5) + 1,"People");
-//            people.add(person);
-//        }
-//
-//        System.out.println(people);
+            System.out.println(set);
+        }
+
+        HashSet<Person> people = new HashSet<>();
+        for(int i = 0; i < 10; i++) {
+            Person person = new Person((int) (Math.random() * 5) + 1,"People");
+            people.add(person);
+        }
+
+        System.out.println(people);
 
         TreeSet<Integer> treeSet = new TreeSet<>();
         for(int i = 0; i < 10; i++) {
@@ -60,6 +61,25 @@ public class SetStudy {
         System.out.println(treeSet.subSet(0 , 20)); // 범위 검색
         System.out.println(treeSet.headSet(50)); // 50 보다 작은거
         System.out.println(treeSet.tailSet(50)); // 50 보다 큰거
+
+
+         */
+        String input = "awgawgsrhtdjasdrthasdrjadrhasdg";
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        for(int i = 0; i < input.length(); i++) {
+            String ch = input.charAt(i) + "";
+
+            if(hashMap.containsKey(ch)) {
+                hashMap.put(ch, hashMap.get(ch) + 1);
+            } else {
+                hashMap.put(ch, 1);
+            }
+        }
+
+        for (String key : hashMap.keySet()) {
+            System.out.printf("[%s] : %d%n", key, hashMap.get(key));
+        }
     }
 
 }
