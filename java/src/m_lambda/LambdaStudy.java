6f7ba5575bb6959f03f20c146ea1ffd9731db9cc
5tr1ker <follow2272@naver.com>
2025-12-10 22:07:22 +0900

    feat: 람다식의 활용 학습

--- java/src/m_lambda/LambdaStudy.java
+++ java/src/m_lambda/LambdaStudy.java
@@ -9,13 +9,22 @@ public class LambdaStudy {
         int Calc(int first);
     }
 
-//    public static int max(int a, int b) {
-//        return Math.max(a, b);
-//    }
+    public static Math returnLambda(int index) {
+        return (input) -> index - input;
+    }
+
+    public static void printLambda(Math m) {
+        System.out.println("결과 값 : " + m.Calc(50));
+    }
 
     public static void main(String[] args) {
         Math result = a -> a * a;
         System.out.println(result.Calc(5));
+
+        printLambda((a) -> a * a + 2);
+
+        Math myLambda = returnLambda(50);
+        System.out.println(myLambda.Calc(5));
     }
 
 }
