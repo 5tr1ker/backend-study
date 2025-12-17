2adfbec308e18391c85e1ddda7c44d380e6d913b
5tr1ker <follow2272@naver.com>
2025-12-17 22:07:23 +0900

    feat: 스트림 연산자 학습

--- java/src/n_stream/StreamStudy.java
+++ java/src/n_stream/StreamStudy.java
@@ -22,6 +22,29 @@ public class StreamStudy {
         Random random = new Random();
         IntStream randomStream = random.ints(0 , 50).limit(5);
         randomStream.forEach(System.out::println);
+
+        // generate()
+        Stream.generate(() -> 1).limit(5).forEach(System.out::println);
+        Stream.iterate(5, a -> a + 2).limit(5).forEach(System.out::println);
+
+        Stream<Integer> stream_1 = Stream.iterate(0, a -> a + 1).limit(5);
+        Stream<Integer> stream_2 = Stream.generate(() -> 2).limit(5);
+        Stream<Integer> stream_3 = Stream.concat(stream_1, stream_2);
+        // 0
+        //1
+        //2
+        //3
+        //4
+        //2
+        //2
+        //2
+        //2
+        //2
+
+        stream_3 = stream_3.distinct().filter(a -> a % 2 == 0);
+        stream_3.forEach(System.out::println);
+
+
     }
 
 }
