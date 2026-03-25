769f736969c836b2c654c501ec7f8abf55cf7956
5tr1ker <follow2272@naver.com>
2026-03-25 23:25:58 +0900

    feat: Java 내부 구조 학습 준비

---
+++ java/src/o_jvmtest/JvmTest.java
@@ -0,0 +1,21 @@
+package o_jvmtest;
+
+public class JvmTest {
+
+    public static void main(String[] args) {
+        System.out.println("출력 성공했습니다.");
+    }
+
+    private void oldArea() {
+
+    }
+
+    private void gcTest() {
+
+    }
+
+    private void heapArea() {
+        
+    }
+
+}
