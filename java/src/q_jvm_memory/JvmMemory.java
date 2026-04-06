ed22872638940f293166c92c93db1286da36f90c
5tr1ker <follow2272@naver.com>
2026-04-06 23:05:21 +0900

    feat: jvm 메모리 구조에 대해 심층 학습

---
+++ java/src/q_jvm_memory/JvmMemory.java
@@ -0,0 +1,14 @@
+package q_jvm_memory;
+
+public class JvmMemory {
+
+    public int a = 0; // 인스턴스 변수
+    public static int b = 0; // 클래스 변수
+
+
+    public static void main(String[] args) { // 매개변수
+        int c = 0; // 지역 변수
+
+    }
+
+}
